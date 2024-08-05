ff9ca3d9315252e654824be3a8ac823efec82cfc
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-06 01:46:26 +0300

    1. MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.dbName = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.db = null;
+
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(this.dbName);
+      });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.db) {
+      await this.client.connect();
+      this.db = this.client.db(this.dbName);
+    }
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) {
+      await this.client.connect();
+      this.db = this.client.db(this.dbName);
+    }
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
