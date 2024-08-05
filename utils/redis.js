9f2abeb1d3c6b6be638eae4819dc666d12209f92
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-06 00:26:19 +0300

    0. Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,42 @@
-class RedisClient {
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = true;
+
+    this.client.on('error', (err) => {
+      this.connected = false;
+      console.error('Redis client error:', err);
+    });
+
+    this.asyncGet = promisify(this.client.get).bind(this.client);
+    this.asyncSet = promisify(this.client.set).bind(this.client);
+    this.asyncDel = promisify(this.client.del).bind(this.client);
+
+    this.client.on('ready', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    const value = await this.asyncGet(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.asyncSet(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.asyncDel(key);
+  }
 }
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
